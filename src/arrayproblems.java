602495d172630bc96d16c31e8446cdb4bd9e9f27
Musab <joiya4636@gmail.com>
2026-06-18 19:03:55 +0500

    Create arrayproblems.java

---
+++ src/arrayproblems.java
@@ -0,0 +1,54 @@
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+
+public class arrayproblems {
+
+    static int removeDuplicates(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+
+        }
+        return map.keySet().size();
+
+    }
+    static int findFirstRepeatingElement(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:arr){
+            if(map.get(i)>1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    static int findPivotIndex(int[] arr){
+        int[] leftSum=new int[arr.length];
+        int[] rightSum=new int[arr.length];
+        leftSum[0]=arr[0];
+        rightSum[arr.length-1]=arr[arr.length-1];
+        for(int i=1;i<arr.length;i++){
+            leftSum[i]=leftSum[i-1]+arr[i];
+        }
+        for(int i=arr.length-2;i>=0;i--){
+           rightSum[i]=rightSum[i+1]+arr[i];
+        }
+        for(int i=0;i<arr.length;i++){
+            if(leftSum[i]==rightSum[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    static void main() {
+        int[] arr={1,7,3,6,5,6};
+//        System.out.println(removeDuplicates(arr));
+//        System.out.println(findFirstRepeatingElement(arr));
+        System.out.println(findPivotIndex(arr));
+
+
+    }
+}
